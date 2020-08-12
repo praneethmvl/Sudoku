01d0a72ba2aeb438af327687d554e138d9f54f93
Shreemit <shreemit27@gmail.com>
2020-08-12 22:50:59 +0530

    Displaying Grid

---
+++ app_class.py
@@ -0,0 +1,43 @@
+import pygame
+from settings import *
+import sys
+class App:
+    def __init__(self):
+        pygame.init()
+        self.window = pygame.display.set_mode((WIDTH,HEIGHT))
+        self.running = True
+        self.grid = testBoard
+
+
+    def run(self):
+        while self.running:
+            self.events()
+            self.update()
+            self.draw()
+        pygame.quit()
+        sys.exit()
+
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.window.fill(WHITE)
+        self.drawGrid(self.window)
+        pygame.display.update()
+
+
+    def drawGrid(self,window):
+        pygame.draw.rect(window,BLACK,(gridPos[0],gridPos[1],WIDTH - 150,HEIGHT - 150),2)
+        for x in range(9):
+            if x%3 == 0:
+                pygame.draw.line(window,BLACK,(gridPos[0]+(x*cellSize ), gridPos[1]) , (gridPos[0]+(x*cellSize ), gridPos[1]+450),2)
+                pygame.draw.line(window, BLACK, (gridPos[0], gridPos[1] +(x*cellSize)), (gridPos[0]+450, gridPos[1]+(x*cellSize)),2)
+            else:
+                pygame.draw.line(window,BLACK,(gridPos[0]+(x*cellSize ), gridPos[1]) , (gridPos[0]+(x*cellSize ), gridPos[1]+450))
+                pygame.draw.line(window, BLACK, (gridPos[0], gridPos[1] +(x*cellSize)), (gridPos[0]+450, gridPos[1]+(x*cellSize)))
